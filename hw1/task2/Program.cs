302d169de83c50cc3a266d6df0fea4064568770d
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-14 22:55:00 +0300

    hw2

---
+++ hw1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Enter three numbers one by one:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b )
+{
+    System.Console.WriteLine("FirstNum = maximum");
+}
+
+else if ( b > c)
+{ 
+     System.Console.WriteLine("SecondNum = maximum");
+     
+}
+else 
+{
+     System.Console.WriteLine("ThirdNum = maximum");
+}
