7cebadbb700d32e503802861ed0d0bbf709bb8ba
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-14 22:23:26 +0300

    hw1

---
+++ hw1/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Enter two numbers one by one:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if ( a > b )
+{
+    int max = a;
+    int min = b;
+   System.Console.WriteLine("FirstNum = maximum");
+   System.Console.WriteLine("SecondNum = minimum");
+}
+else
+{ 
+   System.Console.WriteLine("FirstNum = minimum");
+   System.Console.WriteLine("SecondNum = maximum");
+}
