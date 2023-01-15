720e998e79e81f0c903f1f8313959afd7c8340ac
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-15 11:21:12 +0300

    hw3

---
+++ hw1/task3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Enter an enteger:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if ( (a % 2)! == 0)
+{
+   System.Console.WriteLine("Yes"); 
+}
+else
+{
+System.Console.WriteLine("No"); 
+}
