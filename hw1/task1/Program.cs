d16e4730d93e4e4f2cf4f34063469529b7022568
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-15 20:02:16 +0300

    add minimum

--- hw1/task1/Program.cs
+++ hw1/task1/Program.cs
@@ -1,13 +1,16 @@
 ﻿System.Console.WriteLine("Enter two numbers one by one:");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-if ( a > b )
-{
-   System.Console.WriteLine("FirstNum = maximum");
-   System.Console.WriteLine("SecondNum = minimum");
-}
-else
-{ 
-   System.Console.WriteLine("FirstNum = minimum");
-   System.Console.WriteLine("SecondNum = maximum");
-}
+int max = a;
+int min = b;
+
+if ( a > b ) max = a;
+
+else max = b;
+             
+if (b > a) min = a;
+
+System.Console.Write ("maximum:");
+System.Console.WriteLine (max);
+System.Console.Write ("minimum:");
+System.Console.WriteLine (min);
