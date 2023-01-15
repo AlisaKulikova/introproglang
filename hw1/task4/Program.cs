afda51930b868a64f9bcdb4ff3dff4f6535d6f8f
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-15 12:41:21 +0300

    hw4

---
+++ hw1/task4/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Console.WriteLine("Enter an enteger from 1:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+ 
+while (i <= a)
+{
+    System.Console.WriteLine(i + " ");
+    i = i + 2;
+}
+ 
+
+  
+
