41dd7d088891ba71a9111509251d3bc19c2c7a28
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-17 20:29:14 +0300

    hw2task2

---
+++ hw2/task2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int ThirdFunction(int a)
+{
+int thirdnum = -1;
+
+if (a >= 100)
+{
+while (a > 999)
+{
+    a = a/10;
+}
+thirdnum = a%10;
+}
+return thirdnum;
+
+}
+Console.WriteLine("Enter an integer:");
+int a1 = Convert.ToInt32(Console.ReadLine());
+if (ThirdFunction(a1)== -1) 
+{
+Console.WriteLine("There is no third number");
+}
+else 
+{
+    Console.WriteLine(ThirdFunction(a1));
+}
+
+
+
