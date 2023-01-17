a53102e031f5c9de04d6ba39f6887f679e4fef63
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-17 21:54:04 +0300

    1

---
+++ hw2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter a three-digit number:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (99 < a && a < 1000)
+{
+    int secondnum = (a / 10)%10;
+    Console.WriteLine(secondnum);
+}
+else
+{
+    Console.WriteLine("Enter ONLY THREE-DIGIT number, please");
+}
