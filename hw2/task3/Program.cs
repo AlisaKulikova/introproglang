2f3be3d72e57a683780d588600f74cab810b6f4e
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-17 21:48:00 +0300

    hw2task31

---
+++ hw2/task3/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.WriteLine("Is this day a holyday?");
+Console.WriteLine("p.s.: enter an integer from 1 to 7:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+switch (a)
+{
+    case 1:
+    Console.WriteLine("Monday--> no, it's not");
+    Console.WriteLine("GO TO WORK!");
+    break;
+
+    case 2:
+    Console.WriteLine("Tuesday-->no, it's not");
+    Console.WriteLine("GO TO WORK!");
+    break;
+    
+    case 3:
+    Console.WriteLine("Wednesday--> no, it's not");
+    Console.WriteLine("GO TO WORK!");
+    break;
+    
+    case 4:
+    Console.WriteLine("Thursday--> no, it's not");
+    Console.WriteLine("GO TO WORK!");
+    break;
+    
+    case 5:
+    Console.WriteLine("Friday--> no, it's not");
+    Console.WriteLine("GO TO WORK!");
+    break;
+    
+    case 6:
+    Console.WriteLine("Saturday --> yes, it is");
+    Console.WriteLine("YOU CAN KEEP SLEEPING");
+    break;
+   
+    case 7:
+    Console.WriteLine("Sunday--> yes, it is");
+    Console.WriteLine("YOU CAN KEEP SLEEPING");
+    break;
+
+    default:
+    Console.WriteLine("Enter an integer FROM 1 TO 7 ONLY");
+    break;
+}
