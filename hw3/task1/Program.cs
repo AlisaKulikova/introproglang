b4f2175a12b7322fe65cf49f3887c8b94289f449
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-19 22:11:11 +0300

    hw3/1

---
+++ hw3/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿int Function(int num)
+{
+
+  int n = num;
+  int revers = 0;
+   while (n>0)
+   {
+    int lastnum = n%10;
+    revers= revers*10+lastnum;
+    n=n/10;
+   }  
+   return(num = revers);
+}
+
+Console.WriteLine("Enter an integer:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+if (Function(n1)==n1) 
+{
+   Console.WriteLine(" PALINDROME");
+}
+else
+{
+ Console.WriteLine("NOT PALINDROME");
+}
+
