c8c4df11451884c5d9c1a429f1a4dfba2b087634
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-24 22:10:00 +0300

    hw4/2

---
+++ hw4/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int SumFunction(int a)
+{
+int digit= 0; 
+int sum = 0;
+while (a>0)
+{
+ digit = a%10;
+ sum= sum+digit;
+ a=a/10;   
+}
+return(sum);
+}
+Console.WriteLine("Enter an integer:");
+int a =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumFunction(a));
