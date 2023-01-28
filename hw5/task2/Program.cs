1816d98555f5344c2af3e9d23a7ea2d6d58a3542
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-28 22:43:20 +0300

    hw5\2

---
+++ hw5/task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Entet the number of array elements:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+Random rnd = new Random();
+
+for (int i=0; i<array.Length; i++)
+{
+array[i]= rnd.Next(-100,101);
+Console.Write( array[i]+" ");
+}
+int sum = 0;
+for (int i=0; i<array.Length; i++)
+{
+    if (i%2==1)
+    {
+        sum=sum+array[i];
+        i++;
+    }
+}
+Console.WriteLine("\nThe sum of array elements in odd position:");
+Console.WriteLine(sum);
