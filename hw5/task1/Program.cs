03fd06bd9ae8571770cc7eecd9352225b4d89e7b
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-01-28 22:43:52 +0300

    hw5\1

---
+++ hw5/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Entet the number of array elements:");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+Random rnd = new Random();
+
+for (int i=0; i<array.Length; i++)
+{
+array[i]= rnd.Next(100,1000);
+Console.Write( array[i]+" ");
+}
+
+int count = 0;
+
+foreach (var item in array)
+{
+    if (item%2==0)
+    {
+        count++;
+    }
+
+}
+Console.WriteLine("\nThe number of even array elements:");
+Console.WriteLine(count);
