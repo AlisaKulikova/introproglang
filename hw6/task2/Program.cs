ce362b58599f70a19420b3d43eb7f63d3eb4a257
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-02 17:07:28 +0300

    hw6\2

---
+++ hw6/task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter value B1:");
+double ba = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value K1:");
+double ka = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value B2:");
+double bb = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter value K2:");
+double kb = Convert.ToInt32(Console.ReadLine());
+if (ka==kb | ba==bb ) Console.WriteLine("Lines don't intersect");
+else 
+{
+double x= (bb-ba)/(ka-kb);
+double y = kb*x+bb;
+Console.Write("Point of intersectoin of lines:");
+Console.Write("(" + Math.Round(x,2)+","+ Math.Round(y,2)+ ")");
+}
