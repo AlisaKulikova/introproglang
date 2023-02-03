4fbbbb9ec45ff6a9157e1bf67860fb9230bd636b
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-03 21:39:09 +0300

    hw7\1

---
+++ hw7/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Enter the number of lines:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of columns:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double [,] array= new double[m,n];
+
+double genRndDouble( int a, int b)
+{
+    Random rnd = new Random();
+    double rndDouble = a+rnd.NextDouble()*(b-a);
+    return rndDouble;
+}
+
+
+void FillArray(double [,]array)
+{
+  for(int i=0;i<array.GetLength(0);i++)
+{
+    for(int j = 0;j<array.GetLength(1);j++)
+    {
+        array[i,j]=genRndDouble(-10,11);
+    }
+}
+}
+
+void PrintArray(double[,] array)
+{
+    for(int i=0;i<array.GetLength(0);i++)
+{
+    for(int j = 0;j<array.GetLength(1);j++)
+    {
+        Console.Write($"{Math.Round(array[i,j],2)}  ");
+    }
+    Console.WriteLine();
+}
+}
+FillArray(array);
+Console.WriteLine("Random array:");
+PrintArray(array);
