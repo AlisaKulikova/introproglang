45ffe3fca1201a839ec5f8a12192c6e50d9890fb
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-05 19:40:29 +0300

    hw7\2

--- hw7/task2/Program.cs
+++ hw7/task2/Program.cs
@@ -38,24 +38,30 @@ FillArray(array);
 Console.WriteLine("Random array:");
 PrintArray(array);
 
-Console.WriteLine("Enter the index of lines:");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Enter the index of columns:");
-int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число:");
+int element = Convert.ToInt32(Console.ReadLine());
 
-if (a>=m || b>=n) Console.WriteLine("The element corresponding to the given indexs does not exist");
+if (array[i,j]==element)
+Console.WriteLine(i,j);
+else Console.WriteLine("такого элеьента не");
 
-else
-   for (int i = 0; i < array.GetLength(0); i++)
-        {
-            for (int j = 0; j < array.GetLength(1); j++)
-            {
+
+// Console.WriteLine("Enter the index of columns:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a>=m || b>=n) Console.WriteLine("The element corresponding to the given indexs does not exist");
+
+// else
+//    for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//             {
                 
-               if(i==a && j==b){Console.Write(String.Format("{0,4}",array[a,b]));}
-               else Console.Write(String.Format("{0,4}"," ♥ ")); 
-            }
+//                if(i==a && j==b){Console.Write(String.Format("{0,4}",array[a,b]));}
+//                else Console.Write(String.Format("{0,4}"," ♥ ")); 
+//             }
            
-            Console.WriteLine();
-        }
+//             Console.WriteLine();
+//         }
 
        
