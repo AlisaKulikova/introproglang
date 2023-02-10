b291fff8382dd6c3cb15c3b228308f6fe304dd41
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-10 19:46:59 +0300

    hw9\1

---
+++ hw9/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Random rnd = new Random();
+int n = rnd.Next(5, 30);
+
+string Recursion(int n)
+{
+    if (n <=0) return "";
+    else return Recursion(n-1)+" "+n;
+}
+Console.WriteLine(Recursion(n));
