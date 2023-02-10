f63e52a51c5373df6f364fb846951a2a51a0ac3a
Alisa Kulikova <mrs.alisakulikova@gmail.com>
2023-02-10 21:33:04 +0300

    hw9\2

---
+++ hw9/task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Random rnd = new Random();
+int n = rnd.Next(5, 30);
+int m = rnd.Next(5, 30);
+while (n >= m)
+{
+    m = rnd.Next(3, 10);
+    n = rnd.Next(3, 10);
+}
+Console.WriteLine("The sum from " + n + " to " + m + " is:");
+
+int Recursion(int n, int m)
+{
+    if (n == m + 1) return 0;
+    else return n + Recursion(n + 1, m);
+}
+Console.WriteLine(Recursion(n, m));
+
